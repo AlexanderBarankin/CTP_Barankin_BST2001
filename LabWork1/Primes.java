fa5b7d84588e4d7a35e422a1b6a4edab81647625
AlexanderBarankin <barcatsash@yandex.ru>
2022-02-14 16:01:35 +0300

    Program Primes was written

--- LabWork1/Primes.java
+++ LabWork1/Primes.java
@@ -1,7 +1,20 @@
+/*
+Класс Primes используется для решения задачи 1. Простые числа.
+В данном классе используются методы, которые находят и выводят простые числа, меньшие 100.
+ */
 public class Primes {
-    public static void main(String[] args){
-        public static boolean isPrime(int n){
-            
+    public static void main(String[] args){ //В методе main() находятся простые числа < 100 при помощи метода isPrime()
+        for (int i = 2; i <= 100; i++){
+            if (isPrime(i))
+                System.out.println(i + " is prime number");
         }
+    }
+    public static boolean isPrime(int n){ //Метод isPrime() определяет, является ли число простым
+        boolean isPrime = true;
+        for (int i = 2; i < n; i++){
+            if ((n % i) == 0)
+                isPrime = false;
+        }
+        return isPrime;
     }
 }
